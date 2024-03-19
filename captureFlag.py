f4485ea95c25a619c8d2143005a1ae7dec4e16d5
Mario Rodriguez Martinez <mariog.rodriguezm@gmail.com>
2024-03-19 13:31:50 +0100

    SSL

--- captureFlag.py
+++ captureFlag.py
@@ -1,12 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 import os
+import urllib3
+import json
+
+# Deshabilitar las advertencias de solicitud HTTPS no verificada
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # URL de la página a raspar
 url = 'https://www.hacktonic.net:23666/'
 
 try:
-    # Realizar la solicitud HTTP sin verificar el certificado SSL
+    # Realizar la solicitud HTTP con verificación de certificado SSL
     response = requests.get(url, verify=False)
 
     # Verificar si la solicitud fue exitosa (código de estado 200)
@@ -27,12 +32,12 @@ try:
             if file_to_find in href:
                 # Construir la URL completa del archivo
                 file_url = url + href
-                # Realizar la solicitud HTTP para descargar el archivo sin verificar el certificado SSL
+                # Realizar la solicitud HTTP para descargar el archivo
                 file_response = requests.get(file_url, verify=False)
                 # Verificar si la solicitud fue exitosa
                 if file_response.status_code == 200:
                     # Especificar la ruta donde guardar el archivo descargado
-                    save_path = os.path.join(r"C:\Users\MarioG\OneDrive\Escritorio", file_to_find)
+                    save_path = os.path.join(os.path.expanduser("~"), "Desktop", file_to_find)
                     # Escribir el contenido del archivo en el disco
                     with open(save_path, 'wb') as f:
                         f.write(file_response.content)
